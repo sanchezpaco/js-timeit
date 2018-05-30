5cc2b9c69fe2c1683288502c932be81041ba1057
manuelgarcia <manuel.garcia@gnommo.com>
2018-05-30 16:57:09 +0200

    :bug: Fixed parseTime function (with momentJS) :sparkles: Added new test

--- spec/timeit_spec.js
+++ spec/timeit_spec.js
@@ -1,46 +1,62 @@
-'use strict'
+"use strict";
 
-const timeIt = require('../lib/timeit')
-const expect = require('chai').expect
+const timeIt = require("../lib/timeit");
+const expect = require("chai").expect;
 
-describe('TimeIt module', () => {
-  describe('#parseTime', () => {
-    it('should calculate the passed time', () => {
-
-      let timeInMilliseconds = 600000
+describe("TimeIt module", () => {
+  describe("#parseTime", () => {
+    it("should calculate the passed time", () => {
+      let timeInMilliseconds = 600000;
 
       let expectedTime = {
-        years:   0,
-        months:  0,
-        weeks:   0,
-        days:    0,
-        hours:   0,
+        years: 0,
+        months: 0,
+        weeks: 0,
+        days: 0,
+        hours: 0,
         minutes: 10,
-        seconds: 0,
-      }
+        seconds: 0
+      };
 
-      let result = timeIt.parseTime(timeInMilliseconds)
+      let result = timeIt.parseTime(timeInMilliseconds);
 
-      expect(result).to.deep.equal(expectedTime)
-    })
+      expect(result).to.deep.equal(expectedTime);
+    });
 
-    it('should calculate the passed time', () => {
-
-      let timeInMilliseconds = 6000000
+    it("should calculate the passed time", () => {
+      let timeInMilliseconds = 6000000;
 
       let expectedTime = {
-        years:   0,
-        months:  0,
-        weeks:   0,
-        days:    0,
-        hours:   1,
+        years: 0,
+        months: 0,
+        weeks: 0,
+        days: 0,
+        hours: 1,
         minutes: 40,
-        seconds: 0,
-      }
+        seconds: 0
+      };
 
-      let result = timeIt.parseTime(timeInMilliseconds)
+      let result = timeIt.parseTime(timeInMilliseconds);
 
-      expect(result).to.deep.equal(expectedTime)
-    })
-  })
-})
+      expect(result).to.deep.equal(expectedTime);
+    });
+
+    it("should calculate the passed time", () => {
+      let timeInMilliseconds = 123123858123;
+
+      let expectedTime = {
+        years: 3,
+        months: 10,
+        weeks: 3,
+        days: 24,
+        hours: 1,
+        minutes: 4,
+        seconds: 18
+      };
+
+      let result = timeIt.parseTime(timeInMilliseconds);
+
+      expect(result).to.deep.equal(expectedTime);
+    });
+  });
+});
