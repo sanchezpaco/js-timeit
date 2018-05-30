0974c1aeecbaa5a75cbb4f5d1895e0900db96f16
manuelgarcia <manuel.garcia@gnommo.com>
2018-05-30 16:09:37 +0200

    :bug: Fixed parseTime function

--- spec/timeit_spec.js
+++ spec/timeit_spec.js
@@ -23,5 +23,24 @@ describe('TimeIt module', () => {
 
       expect(result).to.deep.equal(expectedTime)
     })
+
+    it('should calculate the passed time', () => {
+
+      let timeInMilliseconds = 6000000
+
+      let expectedTime = {
+        years:   0,
+        months:  0,
+        weeks:   0,
+        days:    0,
+        hours:   1,
+        minutes: 40,
+        seconds: 0,
+      }
+
+      let result = timeIt.parseTime(timeInMilliseconds)
+
+      expect(result).to.deep.equal(expectedTime)
+    })
   })
 })
